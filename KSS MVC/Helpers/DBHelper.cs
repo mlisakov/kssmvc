12eaa2a82bf58cef85dcecc44913ad565d3bf2b9
mlisakov <maksim.l.isakov@gmail.com>
2014-05-19 19:49:25 +0400

    Добавлена карта для пользователя

--- KSS MVC/Helpers/DBHelper.cs
+++ KSS MVC/Helpers/DBHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
@@ -15,34 +16,32 @@ namespace KSS.Helpers
             _baseModel = new CompanyBaseModel();
         }
 
-        public static Guid GetUserDepartment(Guid userGuid)
+        public static DepartmentState GetEmployeeDepartment(Guid userGuid)
         {
-            var t = from employee in _baseModel.Employees
-                join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
-                from m in employeeStaff.DefaultIfEmpty()
-                where employee.Id == userGuid && m.ExpirationDate == null
-                select m.DepartmentId;
-                return t.FirstOrDefault();
+            return (from employee in _baseModel.Employees
+                    join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
+                    from m in employeeStaff.DefaultIfEmpty()
+                    join depState in _baseModel.DepartmentStates on m.DepartmentId equals depState.Id into departmentState
+                    from ds in departmentState.DefaultIfEmpty()
+                    where employee.Id == userGuid && m.ExpirationDate == null
+                    select ds).First();
         }
 
-        public static Tuple<Guid, string> GetUserDivision(Guid userGuid)
+        public static DivisionState GetEmployeeDivision(Guid userGuid)
         {
-            var t = from employee in _baseModel.Employees
-                join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
-                from m in employeeStaff.DefaultIfEmpty()
-                join departmentState in _baseModel.DepartmentStates on m.DepartmentId equals departmentState.Id into
-                    depState
-                from ds in depState.DefaultIfEmpty()
-                join divisionState in _baseModel.DivisionStates on ds.DivisionId equals divisionState.Id into divState
-                from divS in divState.DefaultIfEmpty()
-                where employee.Id == userGuid && m.ExpirationDate == null && ds.ExpirationDate == null
-                select new {divS.Id, divS.Division};
-            return t.Any()
-                ? new Tuple<Guid, string>(t.First().Id, t.First().Division)
-                : new Tuple<Guid, string>(Guid.Empty, string.Empty);
+            return (from employee in _baseModel.Employees
+                    join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
+                    from m in employeeStaff.DefaultIfEmpty()
+                    join departmentState in _baseModel.DepartmentStates on m.DepartmentId equals departmentState.Id into
+                        depState
+                    from ds in depState.DefaultIfEmpty()
+                    join divisionState in _baseModel.DivisionStates on ds.DivisionId equals divisionState.Id into divState
+                    from divS in divState.DefaultIfEmpty()
+                    where employee.Id == userGuid && m.ExpirationDate == null && ds.ExpirationDate == null
+                    select divS).First();
         }
 
-        public static Tuple<Guid,string> GetUserFullName(string userLogin)
+        public static Tuple<Guid,string> GetEmployeeFullName(string userLogin)
         {
             var t = from employee in _baseModel.Employees
                 where employee.AccountName.Equals(userLogin)
@@ -50,6 +49,58 @@ namespace KSS.Helpers
             return t.Any()
                 ? new Tuple<Guid, string>(t.First().Id, t.First().Name)
                 : new Tuple<Guid, string>(Guid.Empty, string.Empty);
+        }
+
+        public static Employee GetEmployee(Guid employeeGuid)
+        {
+            return (from employee in _baseModel.Employees
+                   where employee.Id.Equals(employeeGuid)
+                   select employee).First();
+        }
+
+        public static PositionState GetEmployeePositionState(Guid employeeGuid)
+        {
+            return (from employee in _baseModel.Employees
+                join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
+                from m in employeeStaff.DefaultIfEmpty()
+                join posState in _baseModel.PositionStates on m.PositionId equals  posState.Id into positionState
+                from ps in positionState.DefaultIfEmpty()
+                select ps
+            ).First();
+        }
+
+        public static List<SpecificStaffPlace> GetEmployeeSpecificStaffPlaces(Guid employeeGuid)
+        {
+            return (from employee in _baseModel.Employees
+                join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
+                from m in employeeStaff.DefaultIfEmpty()
+                join ss in _baseModel.SpecificStaffs on m.Id equals ss.EmployeeId into specificStaff
+                from specStaff in specificStaff.DefaultIfEmpty()
+                join ssp in _baseModel.SpecificStaffPlaces on specStaff.Id equals ssp.SpecificStaffId into
+                    specificStaffPlace
+                from specStaffPlace in specificStaffPlace.DefaultIfEmpty()
+                select specStaffPlace
+                ).ToList();
+        }
+
+        //public static List<SpecificStaff> GetEmployeeSpecificStaffs(Guid employeeGuid)
+        //{
+        //    return (from employee in _baseModel.Employees
+        //            join staff in _baseModel.Staffs on employee.Id equals staff.Id into employeeStaff
+        //            from m in employeeStaff.DefaultIfEmpty()
+        //            join ss in _baseModel.SpecificStaffs on m.Id equals ss.EmployeeId into specificStaff
+        //            from specStaff in specificStaff.DefaultIfEmpty()
+        //            select specStaff
+        //        ).ToList();
+        //}
+
+        public static List<EmployeePlace> GetEmployeePlaces(Guid employeeGuid)
+        {
+           return (from employee in _baseModel.Employees
+                   join ep in _baseModel.EmployeePlaces on employee.Id equals ep.EmployeeId into employeePlase
+                   from empPlace in employeePlase.DefaultIfEmpty()
+                   select empPlace
+                ).ToList();
         }
     }
 }
